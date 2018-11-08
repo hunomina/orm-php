9e83bfdaefed40f120d35c992e59d463178d1cc7
hunomina <hugo.f972@gmail.com>
2018-11-08 17:17:38 +0100

    Add getForeignKeys() and getCollections() methods
    - getForeignKeys() : return the list of property representing foreign keys
    
    - getCollections() : return the list of property representing collections

--- src/Entity/EntityReflexion.php
+++ src/Entity/EntityReflexion.php
@@ -2,6 +2,7 @@
 
 namespace hunomina\Orm\Entity;
 
+use hunomina\Orm\Database\Ddl\PropertyDdl;
 use ReflectionProperty;
 
 class EntityReflexion
@@ -69,5 +70,65 @@ class EntityReflexion
     public function getProperties(): array
     {
         return $this->_properties;
+    }
+
+    /**
+     * @return string[]
+     * @throws EntityException
+     */
+    public function getCollections(): array
+    {
+        $collections = [];
+
+        /** @var ReflectionProperty $property */
+        foreach ($this->getProperties() as $property) {
+            $propertyAnnotation = new PropertyAnnotation($property);
+            if ($annotation = $propertyAnnotation->getAnnotation('Collection')) {
+                if (preg_match(PropertyDdl::COLLECTION_ANNOTATION_REGEXP, $annotation, $match) && $entityClass = $match[1]) {
+                    if (class_exists($entityClass)) {
+                        if (is_subclass_of($entityClass, Entity::class)) {
+                            $collections[$property->name] = $entityClass;
+                        } else {
+                            throw new EntityException("The class '" . $entityClass . "' does not extend '" . Entity::class . "' and thereby can not be used for collections");
+                        }
+                    } else {
+                        throw new EntityException("The class '" . $entityClass . "' does not exist and thereby can not be used for collections");
+                    }
+                } else {
+                    throw new EntityException('The @Collection annotation has not been defined');
+                }
+            }
+        }
+        return $collections;
+    }
+
+    /**
+     * @return string[]
+     * @throws EntityException
+     */
+    public function getForeignKeys(): array
+    {
+        $foreignKeys = [];
+
+        /** @var ReflectionProperty $property */
+        foreach ($this->getProperties() as $property) {
+            $propertyAnnotation = new PropertyAnnotation($property);
+            if ($annotation = $propertyAnnotation->getAnnotation('ForeignKey')) {
+                if (preg_match(PropertyDdl::FOREIGN_KEY_ANNOTATION_REGEXP, $annotation, $match) && $entityClass = $match[1]) {
+                    if (class_exists($entityClass)) {
+                        if (is_subclass_of($entityClass, Entity::class)) {
+                            $foreignKeys[$property->name] = $entityClass;
+                        } else {
+                            throw new EntityException("The class '" . $entityClass . "' does not extend '" . Entity::class . "' and thereby can not be used for foreign keys");
+                        }
+                    } else {
+                        throw new EntityException("The class '" . $entityClass . "' does not exist and thereby can not be used for foreign keys");
+                    }
+                } else {
+                    throw new EntityException('The @ForeignKey annotation has not been defined');
+                }
+            }
+        }
+        return $foreignKeys;
     }
 }
