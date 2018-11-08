04f9c9b5d5a3467682cd50c3da63345d5e52ee25
hunomina <hugo.f972@gmail.com>
2018-11-08 17:22:04 +0100

    Update namespace and add foreign key and collection loading feature

--- src/EntityRepository/EntityRepository.php
+++ src/EntityRepository/EntityRepository.php
@@ -1,12 +1,14 @@
 <?php
 
-namespace hunomina\EntityRepository;
+namespace hunomina\Orm\EntityRepository;
 
 use hunomina\Orm\Database\QueryBuilder\QueryBuilderException;
 use hunomina\Orm\Database\QueryBuilder\QueryBuilderFactory;
 use hunomina\Orm\Database\Statement\StatementException;
 use hunomina\Orm\Database\Statement\StatementFormatter;
 use hunomina\Orm\Entity\Entity;
+use hunomina\Orm\Entity\EntityException;
+use hunomina\Orm\Entity\EntityReflexion;
 use PDO;
 
 abstract class EntityRepository
@@ -38,12 +40,14 @@ abstract class EntityRepository
 
     /**
      * @param int $id
+     * @param bool $load
      * @return Entity|null
+     * @throws EntityException
      * @throws EntityRepositoryException
      * @throws QueryBuilderException
      * @throws StatementException
      */
-    public function find(int $id): ?Entity
+    public function find(int $id, bool $load = true): ?Entity
     {
         $queryBuilder = QueryBuilderFactory::get($this->getEntityTable(), $this->_type);
         $query = $queryBuilder->select()->where('id = :id')->execute();
@@ -52,21 +56,37 @@ abstract class EntityRepository
         $statement->execute();
 
         $results = $this->_statement_formatter->format($statement)->fetchObject($this->getEntityClass());
-        return \count($results) > 0 ? $results[0] : null;
+
+        if (\count($results) > 0) {
+            $entity = $results[0];
+            if ($load) {
+                $this->load($entity);
+            }
+            return $entity;
+        }
+        return null;
     }
 
     /**
+     * @param bool $load
      * @return array
+     * @throws EntityException
      * @throws EntityRepositoryException
      * @throws QueryBuilderException
      * @throws StatementException
      */
-    public function findAll(): array
+    public function findAll(bool $load = true): array
     {
         $queryBuilder = QueryBuilderFactory::get($this->getEntityTable(), $this->_type);
         $query = $queryBuilder->select()->execute();
         $statement = $this->_pdo->query($query);
-        return $this->_statement_formatter->format($statement)->fetchObject($this->getEntityClass());
+        $results = $this->_statement_formatter->format($statement)->fetchObject($this->getEntityClass());
+        if ($load) {
+            foreach ($results as $result) {
+                $this->load($result);
+            }
+        }
+        return $results;
     }
 
     abstract protected function getEntityClass(): string;
@@ -86,5 +106,99 @@ abstract class EntityRepository
             throw new EntityRepositoryException('The Entity Repository can only use Entity classes');
         }
         throw new EntityRepositoryException('The `' . $entityClass . '` class does not exist');
+    }
+
+    /**
+     * @param Entity $entity
+     * @throws EntityException
+     * @throws EntityRepositoryException
+     * @throws QueryBuilderException
+     * @throws StatementException
+     */
+    final protected function load(Entity $entity): void
+    {
+        $this->loadForeignKeys($entity);
+        $this->loadCollections($entity);
+    }
+
+    /**
+     * @param Entity $entity
+     * @throws EntityException
+     * @throws EntityRepositoryException
+     * @throws QueryBuilderException
+     * @throws StatementException
+     */
+    final protected function loadForeignKeys(Entity $entity): void
+    {
+        $reflexion = new EntityReflexion(\get_class($entity));
+        $foreignKeys = $reflexion->getForeignKeys();
+
+        $builder = QueryBuilderFactory::get($this->getEntityTable(), $this->_type);
+
+        /** @var Entity $class */
+        foreach ($foreignKeys as $property => $class) {
+
+            $query = $builder->select()->where('id = :id')->setTable($class::getTable());
+            $statement = $this->_pdo->prepare($query->execute());
+            $statement->bindParam(':id', $entity->{$property}, PDO::PARAM_INT);
+            $statement->execute();
+
+            $results = $this->_statement_formatter->format($statement)->fetchObject($class);
+            if (\count($results) > 0) {
+                $foreignKey = $results[0];
+                $this->load($foreignKey);
+                $entity->{$property} = $foreignKey;
+            } else {
+                throw new EntityRepositoryException('The `' . $class . '` entity with id=' . $entity->{$property} . ' has not been found');
+                // or null
+            }
+        }
+    }
+
+    /**
+     * @param Entity $entity
+     * @throws EntityException
+     * @throws EntityRepositoryException
+     * @throws QueryBuilderException
+     * @throws StatementException
+     */
+    final protected function loadCollections(Entity $entity): void
+    {
+        $reflexion = new EntityReflexion(\get_class($entity));
+        $collections = $reflexion->getCollections();
+
+        $builder = QueryBuilderFactory::get($this->getEntityTable(), $this->_type);
+
+        /** @var Entity $class */
+        foreach ($collections as $property => $class) {
+
+            $query = $builder->select()
+                ->where($entity::getTable() . ' = :' . $entity::getTable())
+                ->setTable($entity::getTable() . '_' . $property)
+                ->execute();
+
+            $statement = $this->_pdo->prepare($query);
+            $statement->bindParam(':' . $entity::getTable(), $entity->id, PDO::PARAM_INT);
+            $statement->execute();
+
+            $results = $this->_statement_formatter->format($statement)->fetchAssoc();
+
+            $collectionIds = [];
+            $collectionItemTable = $class::getTable();
+            foreach ($results as $result) {
+                if (!\in_array((int)$result[$collectionItemTable], $collectionIds, true)) {
+                    $collectionIds[] = (int)$result[$collectionItemTable];
+                }
+            }
+
+            $query = $builder->select()
+                ->where('id IN (' . implode(', ', $collectionIds) . ')')
+                ->setTable($class::getTable())
+                ->execute();
+
+            $statement = $this->_pdo->query($query);
+            $results = $this->_statement_formatter->format($statement)->fetchObject($class);
+            $entity->{$property} = $results;
+        }
     }
 }
